1479ee38fead3aa940cd9c765941fdc73f269b77
Ars_Nikon <nik.arseniy01@gmail.com>
2023-12-19 21:02:16 +0300

    Добавьте файлы проекта.

---
+++ MetricsApp.AppHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var grafana = builder.AddContainer("grafana", "grafana/grafana")
+                     .WithVolumeMount("../grafana/config", "/etc/grafana")
+                     .WithVolumeMount("../grafana/dashboards", "/var/lib/grafana/dashboards")
+                     .WithServiceBinding(containerPort: 3000, hostPort: 3000, name: "grafana-http", scheme: "http");
+
+
+
+var sql = builder.AddPostgresContainer("pg").AddDatabase("sqldata");
+
+builder.AddProject<Projects.MetricsApp>("aspiresql")
+    .WithEnvironment("GRAFANA_URL", grafana.GetEndpoint("grafana-http"))
+    .WithReference(sql);
+
+
+
+builder.AddContainer("prometheus", "prom/prometheus")
+       .WithVolumeMount("../prometheus", "/etc/prometheus")
+       .WithServiceBinding(9090, hostPort: 9090);
+
+using var app = builder.Build();
+
+await app.RunAsync();
---
+++ MetricsApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MetricsApp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddNpgsqlDataSource("sqldata");
+builder.AddNpgsqlDbContext<AppDbContext>("sqldata");
+
+builder.AddServiceDefaults();
+
+builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddAuthorizationBuilder();
+
+
+
+builder.Services.AddIdentityCore<MyUser>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddApiEndpoints();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+app.MapFallbackToFile("index.html");
+
+var api = app.MapGroup("/api");
+api.MapIdentityApi<MyUser>();
+api.MapClientApi(app.Configuration);
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseStaticFiles();
+app.UseBlazorFrameworkFiles();
+
+await Auth.InitializeTestUserAsync(app.Services);
+
+app.Run();
+
